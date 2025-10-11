c352fd9f5d303c934aee7617e817b3765234b393
Asma <asmaanwar1126@gmail.com>
2025-10-11 23:08:10 +0600

    added useCallback folder with component

---
+++ useCallback/use-calllback/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState, useCallback } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+  const [adjective, setAdjective] = useState("Good")
+  const getAdjective =useCallback(
+    () => {
+      return "Awesome"+count2
+    },
+    [count],
+  )
+  
+  return (
+    <>
+      <Navbar adjective={"Good"} getAdjective={getAdjective} />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>  
+          <button onClick={() => setCount2((count2) => count2 + 1)}>
+          count2 is {count2}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
